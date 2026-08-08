89fa5b68726455ee2d48a63bdc8cb03dc710234b
solmoviesbrasil <solmoviesbrasil@gmail.com>
2026-08-08 10:14:50 -0400

    atualização nivel shad

--- js/ajuda.js
+++ js/ajuda.js
@@ -461,47 +461,6 @@ wrapper.style.cssText = `
     gap:4px;
     width:100%;
 `;
-// // Bloqueia atalhos comuns do DevTools
-// document.addEventListener("keydown", (e) => {
-
-//     // F12
-//     if (e.key === "F12") {
-//         e.preventDefault();
-//         return false;
-//     }
-
-//     // Ctrl + Shift + I
-//     if (e.ctrlKey && e.shiftKey && e.key.toLowerCase() === "i") {
-//         e.preventDefault();
-//         return false;
-//     }
-
-//     // Ctrl + Shift + J
-//     if (e.ctrlKey && e.shiftKey && e.key.toLowerCase() === "j") {
-//         e.preventDefault();
-//         return false;
-//     }
-
-//     // Ctrl + Shift + C
-//     if (e.ctrlKey && e.shiftKey && e.key.toLowerCase() === "c") {
-//         e.preventDefault();
-//         return false;
-//     }
-
-//     // Ctrl + U (Ver código-fonte)
-//     if (e.ctrlKey && e.key.toLowerCase() === "u") {
-//         e.preventDefault();
-//         return false;
-//     }
-
-//     // Ctrl + S
-//     if (e.ctrlKey && e.key.toLowerCase() === "s") {
-//         e.preventDefault();
-//         return false;
-//     }
-
-// });
-
 
 // ==========================================================
 // PRIMEIRA LINHA
